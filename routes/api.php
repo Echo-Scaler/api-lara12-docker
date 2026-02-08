340fae98bdb0c247afe091a0f652061002db7f94
Wai Yan <waiyan.msc.yu12@gmail.com>
2026-02-09 00:27:26 +0900

    api_process_start

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'Api is working']);
-});
+// Route::get('/test', function () {
+//     return response()->json(['message' => 'Api is working']);
+// });
+
+Route::get('/test', [PostController::class, 'index']);
