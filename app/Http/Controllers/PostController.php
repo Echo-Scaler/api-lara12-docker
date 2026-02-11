7887b7f2dd6d53616f03d657a85deb0242ba8e02
Wai Yan <waiyan.msc.yu12@gmail.com>
2026-02-12 00:20:49 +0900

    crude-api-create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,47 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        // return response()->json([
-        //     'message' => 'Api is working'
-        // ]);
+        $posts = Post::all();
+        // dd($posts);
+        return response()->json([
+            'message' => 'Posts retrieved successfully',
+            'posts' => $posts
+        ], 200);
     }
 
     public function show($id)
     {
-        // return response()->json([
-        //     'message' => 'Api is working',
-        //     'id' => $id
-        // ]);
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not found'
+            ], 404);
+        }
+        return response()->json([
+            'message' => 'Post retrieved successfully',
+            'post' => $post
+        ], 200);
     }
 
     public function store(Request $request)
     {
-        // return response()->json([
-        //     'message' => 'Api is working',
-        //     'data' => $request->all()
-        // ]);
+        // dd($request->all());
+        $post = Post::create(([
+            'title' => $request->title,
+            'content' => $request->content
+        ]));
+        return response()->json([
+            'message' => 'Post created successfully',
+            // 'post' => $post
+        ], 201);
     }
 
     public function update(Request $request, $id)
     {
-        // return response()->json([
-        //     'message' => 'Api is working',
-        //     'id' => $id,
-        //     'data' => $request->all()
-        // ]);
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not found'
+            ], 404);
+        }
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+        return response()->json([
+            'message' => 'Post updated successfully',
+            // 'post' => $post
+        ], 200);
     }
 
     public function destroy($id)
     {
-        // return response()->json([
-        //     'message' => 'Api is working',
-        //     'id' => $id
-        // ]);
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not found'
+            ], 404);
+        }
+        $post->delete();
+        return response()->json([
+            'message' => 'Post deleted successfully',
+            // 'post' => $post
+        ], 200);
     }
 }
